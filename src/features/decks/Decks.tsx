88229420c912fb2a8b3146ee03563f20e317fdad
ShepLT1 <luke.t.shepherd@gmail.com>
2023-10-19 16:28:55 -0700

    Implement PageHeader component and remove unused component imports in decks feature

--- src/features/decks/Decks.tsx
+++ src/features/decks/Decks.tsx
@@ -1,18 +1,12 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useAppSelector, useAppDispatch } from "../../app/hooks"
 import { selectDecks, getAllDecks } from "./decksSlice"
-import {
-  Grid,
-  List,
-  ListItemButton,
-  ListItemText,
-  Typography,
-  Button,
-} from "@mui/material"
+import { Grid, List, ListItemButton, ListItemText } from "@mui/material"
 import { Link } from "react-router-dom"
 import { getDeckById } from "../deck/deckSlice"
 import BaseModal from "../baseModal/BaseModal"
 import { DeckForm } from "../deck/DeckForm"
+import { PageHeader } from "../../components/pageHeader"
 import { updateBaseModal } from "../baseModal/baseModalSlice"
 
 export function Decks() {
@@ -25,29 +19,11 @@ export function Decks() {
 
   return (
     <Grid container width="100%">
-      <Grid
-        container
-        sx={{ marginTop: 2, marginBottom: 2 }}
-        alignItems={"center"}
-      >
-        <Grid item xs={10}>
-          <Typography
-            variant="h3"
-            textAlign={"left"}
-            sx={{ paddingLeft: 4, paddingTop: 2 }}
-            gutterBottom
-          >
-            Decks
-          </Typography>
-        </Grid>
-        <Button
-          variant="contained"
-          sx={{ height: 40 }}
-          onClick={() => dispatch(updateBaseModal(true))}
-        >
-          Create New Deck
-        </Button>
-      </Grid>
+      <PageHeader
+        title="Decks"
+        buttonTitle="Create New Deck"
+        buttonOnClick={() => dispatch(updateBaseModal(true))}
+      />
       <Grid item xs={12}>
         <List>
           {decks.map((deck) => {
