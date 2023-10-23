d2541e1e4661f22cdd73f2103cc9f3c072258fcf
ShepLT1 <luke.t.shepherd@gmail.com>
2023-10-23 16:51:58 -0700

    Add mana cost deconstructor function

--- src/features/card/helpers.tsx
+++ src/features/card/helpers.tsx
@@ -16,3 +16,20 @@ export const constructNewManaCost = (manaCostObj: {
   }
   return newManaCost
 }
+
+export const deconstructManaCost = (manaCost: string[]) => {
+  const manaCostObj = {
+    white: 0,
+    blue: 0,
+    black: 0,
+    red: 0,
+    green: 0,
+    colorless: 0,
+    any: 0,
+  }
+  manaCost.forEach((mana) => {
+    const key = mana.toLowerCase() as keyof typeof manaCostObj
+    manaCostObj[key]++
+  })
+  return manaCostObj
+}
