d976ffd475cc56bce447dc00d349ae06e5fb1b7e
ShepLT1 <luke.t.shepherd@gmail.com>
2023-10-26 13:26:57 -0700

    Implement search feature on deck page

--- src/features/deck/Deck.tsx
+++ src/features/deck/Deck.tsx
@@ -4,8 +4,9 @@ import EditIcon from "@mui/icons-material/Edit"
 import CheckIcon from "@mui/icons-material/Check"
 import CloseIcon from "@mui/icons-material/Close"
 import { useAppSelector, useAppDispatch } from "../../app/hooks"
-import { selectDeck, updateDeckName } from "./deckSlice"
+import { selectDeck, updateDeckName, addCardInstance } from "./deckSlice"
 import { DeckCardSection } from "../../components/deckCardSection"
+import Search from "../search/Search"
 
 export function Deck() {
   const deck = useAppSelector(selectDeck)
@@ -30,49 +31,50 @@ export function Deck() {
   }, [deck.name])
 
   return (
-    <div>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          {nameEditable ? (
-            <form onSubmit={handleNameSubmit}>
-              <TextField
-                id="deck-name-input"
-                variant="outlined"
-                inputProps={{ style: { fontSize: 50 } }}
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-              />
-              <IconButton aria-label="save" type="submit">
-                <CheckIcon color="success" />
-              </IconButton>
-              <IconButton
-                aria-label="cancel"
-                onClick={() => handleNameCancel()}
-              >
-                <CloseIcon color="warning" />
-              </IconButton>
-            </form>
-          ) : (
-            <Typography variant="h2" gutterBottom>
-              {deck.name}
-              <IconButton
-                aria-label="edit"
-                onClick={() => setNameEditable(true)}
-              >
-                <EditIcon />
-              </IconButton>
-            </Typography>
-          )}
-          <h4>{deck.colors.join(", ")}</h4>
-        </Grid>
-        <Grid item xs={12}>
-          {deck.cardList.map((cardSection, i) => {
-            return (
-              <DeckCardSection key={`card-section-${i}`} {...cardSection} />
-            )
-          })}
-        </Grid>
+    <Grid container width="100%" marginTop={5}>
+      <Grid container display="flex" direction="column" alignItems="start">
+        <Typography variant="h6" marginLeft={6} gutterBottom>
+          Add Cards to Deck
+        </Typography>
+        <Search
+          displayResults={true}
+          onResultClick={(card_id: number) =>
+            dispatch(addCardInstance(card_id))
+          }
+        />
       </Grid>
-    </div>
+      <Grid item xs={12}>
+        {nameEditable ? (
+          <form onSubmit={handleNameSubmit}>
+            <TextField
+              id="deck-name-input"
+              variant="outlined"
+              inputProps={{ style: { fontSize: 50 } }}
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+            />
+            <IconButton aria-label="save" type="submit">
+              <CheckIcon color="success" />
+            </IconButton>
+            <IconButton aria-label="cancel" onClick={() => handleNameCancel()}>
+              <CloseIcon color="warning" />
+            </IconButton>
+          </form>
+        ) : (
+          <Typography variant="h2" gutterBottom>
+            {deck.name}
+            <IconButton aria-label="edit" onClick={() => setNameEditable(true)}>
+              <EditIcon />
+            </IconButton>
+          </Typography>
+        )}
+        <h4>{deck.colors.join(", ")}</h4>
+      </Grid>
+      <Grid item xs={12} marginLeft={5}>
+        {deck.cardList.map((cardSection, i) => {
+          return <DeckCardSection key={`card-section-${i}`} {...cardSection} />
+        })}
+      </Grid>
+    </Grid>
   )
 }
