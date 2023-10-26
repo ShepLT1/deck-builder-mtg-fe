6f68dc23b59f46386801f360f51316ae1e730d5f
ShepLT1 <luke.t.shepherd@gmail.com>
2023-10-26 13:25:34 -0700

    Allow for search to render results

--- src/features/search/Search.tsx
+++ src/features/search/Search.tsx
@@ -1,30 +1,68 @@
-import { useEffect } from "react"
-import { Grid, TextField } from "@mui/material"
+import { useState, useEffect } from "react"
+import {
+  Grid,
+  TextField,
+  Popover,
+  ButtonGroup,
+  Button,
+  Typography,
+} from "@mui/material"
 import { useAppSelector, useAppDispatch } from "../../app/hooks"
 import {
   selectSearch,
   updateSearchInput,
   getCardsByNameContains,
 } from "./searchSlice"
+import { updateCards, getCardsByPage } from "../cards/cardsSlice"
 
-export default function Search() {
+interface SearchProps {
+  displayResults: boolean
+  onResultClick: null | ((card_id: number) => void)
+}
+
+export default function Search(props: SearchProps) {
   const search = useAppSelector(selectSearch)
   const dispatch = useAppDispatch()
 
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
   useEffect(() => {
-    const delayDebounceFn = setTimeout(() => {
+    const delayDebounceFn = setTimeout(async () => {
       if (search.input === "") {
+        if (!props.displayResults) {
+          dispatch(getCardsByPage(0))
+        }
+        if (anchorEl) setAnchorEl(null)
         return
       }
-      dispatch(getCardsByNameContains(search.input))
-    }, 2000)
+      const response = await dispatch(getCardsByNameContains(search.input))
+      if (!props.displayResults) {
+        if (response.meta.requestStatus === "fulfilled") {
+          dispatch(updateCards(response.payload))
+        }
+        return
+      }
+      setAnchorEl(anchorEl ? null : document.getElementById("search-input"))
+    }, 1000)
 
     return () => clearTimeout(delayDebounceFn)
   }, [search.input])
 
+  useEffect(() => {
+    if (props.displayResults && search.input !== "" && !anchorEl) {
+      setAnchorEl(document.getElementById("search-input"))
+    }
+  }, [search.results])
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const open = Boolean(anchorEl)
+
   return (
-    <Grid container>
-      <Grid item xs={12}>
+    <Grid container marginLeft={6}>
+      <Grid item xs={12} display="flex" justifyContent="start">
         <TextField
           id="search-input"
           label="Search (name)"
@@ -33,6 +71,45 @@ export default function Search() {
           onChange={(e) => dispatch(updateSearchInput(e.target.value))}
         />
       </Grid>
+      {props.displayResults ? (
+        <Grid item xs={12}>
+          <Popover
+            id="search-results-popover"
+            open={open}
+            anchorEl={document.getElementById("search-input")}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+          >
+            {search.results.length > 0 ? (
+              <ButtonGroup
+                orientation="vertical"
+                aria-label="vertical contained button group"
+                variant="text"
+              >
+                {search.results.map((result) => {
+                  return (
+                    <Button
+                      key={result.id}
+                      onClick={() => {
+                        props.onResultClick && props.onResultClick(result.id)
+                      }}
+                    >
+                      {result.name}
+                    </Button>
+                  )
+                })}
+              </ButtonGroup>
+            ) : (
+              <Typography sx={{ marginTop: 2 }}>No results found</Typography>
+            )}
+          </Popover>
+        </Grid>
+      ) : (
+        <></>
+      )}
     </Grid>
   )
 }
