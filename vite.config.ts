af8a234a2464325e5d7a2d7d0f77e4dad72e76d3
ShepLT1 <luke.t.shepherd@gmail.com>
2023-12-06 13:08:31 -0800

    Remove local cert references

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
-import fs from "fs"
-// import mkcert from 'vite-plugin-mkcert'
+
+// server: {
+//   https: {
+//     key: fs.readFileSync("./localhost.decrypted.key"),
+//     cert: fs.readFileSync("./localhost.crt"),
+//   },
+// },
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync("./localhost.decrypted.key"),
-      cert: fs.readFileSync("./localhost.crt"),
-    },
-  },
   build: {
     outDir: "build",
     sourcemap: true,
