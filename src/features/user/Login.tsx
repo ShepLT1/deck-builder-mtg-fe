148068c76c821669677a8a66336e7607c34366ef
ShepLT1 <luke.t.shepherd@gmail.com>
2023-12-13 13:21:33 -0800

    Add loading indicator during login/registration loading

--- src/features/user/Login.tsx
+++ src/features/user/Login.tsx
@@ -1,13 +1,16 @@
 import { useState, FormEvent } from "react"
-import { useAppDispatch } from "../../app/hooks"
+import { useAppSelector, useAppDispatch } from "../../app/hooks"
 import { useNavigate } from "react-router-dom"
 import { Grid, Typography, Button, TextField, FormControl } from "@mui/material"
 import { loginUser } from "./userSlice"
 import { setLocalStorageUserId } from "../../utils/persistance/localStorage"
+import { selectLoadingIndicator } from "../loadingIndicator/loadingIndicatorSlice"
+import LoadingIndicator from "../loadingIndicator/LoadingIndicator"
 
 export default function Login() {
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
+  const loadingIndicator = useAppSelector(selectLoadingIndicator)
 
   const [username, setUsername] = useState<string>("")
   const [password, setPassword] = useState<string>("")
@@ -27,52 +30,62 @@ export default function Login() {
   }
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Typography variant="h2" gutterBottom textAlign={"center"}>
-          Log In
-        </Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <form onSubmit={handleUserLogin}>
+    <div>
+      {loadingIndicator.value ? (
+        <LoadingIndicator />
+      ) : (
+        <Grid container spacing={3} marginTop="10vh">
           <Grid item xs={12}>
-            <FormControl sx={{ m: 1, width: 300 }}>
-              <TextField
-                id="username-input"
-                label="Username"
-                variant="outlined"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
-            </FormControl>
+            <Typography variant="h2" gutterBottom textAlign={"center"}>
+              Log In
+            </Typography>
           </Grid>
           <Grid item xs={12}>
-            <FormControl sx={{ m: 1, width: 300 }}>
-              <TextField
-                id="password-input"
-                label="Password"
-                variant="outlined"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </FormControl>
-          </Grid>
-          <Grid container direction="row" justifyContent="center">
-            <Button variant="contained" type="submit">
-              Log in
+            <form onSubmit={handleUserLogin}>
+              <Grid item xs={12}>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                  <TextField
+                    id="username-input"
+                    label="Username"
+                    variant="outlined"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                  <TextField
+                    id="password-input"
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              <Grid container direction="row" justifyContent="center">
+                <Button variant="contained" type="submit">
+                  Log in
+                </Button>
+              </Grid>
+            </form>
+            <Typography
+              variant="body1"
+              textAlign={"center"}
+              sx={{ marginTop: 4 }}
+            >
+              Don't have an account?
+            </Typography>
+            <Button variant="text" onClick={() => navigate("/register")}>
+              Register
             </Button>
           </Grid>
-        </form>
-        <Typography variant="body1" textAlign={"center"} sx={{ marginTop: 4 }}>
-          Don't have an account?
-        </Typography>
-        <Button variant="text" onClick={() => navigate("/register")}>
-          Register
-        </Button>
-      </Grid>
-    </Grid>
+        </Grid>
+      )}
+    </div>
   )
 }
--- src/features/user/Register.tsx
+++ src/features/user/Register.tsx
@@ -1,13 +1,16 @@
 import { useState, FormEvent } from "react"
-import { useAppDispatch } from "../../app/hooks"
+import { useAppSelector, useAppDispatch } from "../../app/hooks"
 import { useNavigate } from "react-router-dom"
 import { Grid, Typography, Button, TextField, FormControl } from "@mui/material"
 import { registerUser, loginUser } from "./userSlice"
 import { setLocalStorageUserId } from "../../utils/persistance/localStorage"
+import { selectLoadingIndicator } from "../loadingIndicator/loadingIndicatorSlice"
+import LoadingIndicator from "../loadingIndicator/LoadingIndicator"
 
 export default function Register() {
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
+  const loadingIndicator = useAppSelector(selectLoadingIndicator)
 
   const [newUsername, setNewUsername] = useState<string>("")
   const [newEmail, setNewEmail] = useState<string>("")
@@ -36,78 +39,84 @@ export default function Register() {
   }
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Typography variant="h2" gutterBottom textAlign={"center"}>
-          Register
-        </Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <form onSubmit={handleUserRegistration}>
+    <div>
+      {loadingIndicator.value ? (
+        <LoadingIndicator />
+      ) : (
+        <Grid container spacing={3} marginTop="10vh">
           <Grid item xs={12}>
-            <FormControl sx={{ m: 1, width: 300 }}>
-              <TextField
-                id="username-input"
-                label="Username"
-                variant="outlined"
-                value={newUsername}
-                onChange={(e) => setNewUsername(e.target.value)}
-                error={newUsername.length > 20}
-                helperText="20 character max"
-                required
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl sx={{ m: 1, width: 300 }}>
-              <TextField
-                id="email-input"
-                label="Email"
-                variant="outlined"
-                type="email"
-                value={newEmail}
-                onChange={(e) => setNewEmail(e.target.value)}
-                error={newEmail.length > 40}
-                helperText="40 character max"
-                required
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl sx={{ m: 1, width: 300 }}>
-              <TextField
-                id="password-input"
-                label="Password"
-                variant="outlined"
-                type="password"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-                required
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl sx={{ m: 1, width: 300 }}>
-              <TextField
-                id="repeat-password-input"
-                label="Re-enter Password"
-                variant="outlined"
-                type="password"
-                value={newPasswordRepeat}
-                onChange={(e) => setNewPasswordRepeat(e.target.value)}
-                required
-                error={newPassword !== newPasswordRepeat}
-                helperText="Passwords must match"
-              />
-            </FormControl>
-          </Grid>
-          <Grid container direction="row" justifyContent="center">
-            <Button variant="contained" type="submit">
+            <Typography variant="h2" gutterBottom textAlign={"center"}>
               Register
-            </Button>
+            </Typography>
           </Grid>
-        </form>
-      </Grid>
-    </Grid>
+          <Grid item xs={12}>
+            <form onSubmit={handleUserRegistration}>
+              <Grid item xs={12}>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                  <TextField
+                    id="username-input"
+                    label="Username"
+                    variant="outlined"
+                    value={newUsername}
+                    onChange={(e) => setNewUsername(e.target.value)}
+                    error={newUsername.length > 20}
+                    helperText="20 character max"
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                  <TextField
+                    id="email-input"
+                    label="Email"
+                    variant="outlined"
+                    type="email"
+                    value={newEmail}
+                    onChange={(e) => setNewEmail(e.target.value)}
+                    error={newEmail.length > 40}
+                    helperText="40 character max"
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                  <TextField
+                    id="password-input"
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                  <TextField
+                    id="repeat-password-input"
+                    label="Re-enter Password"
+                    variant="outlined"
+                    type="password"
+                    value={newPasswordRepeat}
+                    onChange={(e) => setNewPasswordRepeat(e.target.value)}
+                    required
+                    error={newPassword !== newPasswordRepeat}
+                    helperText="Passwords must match"
+                  />
+                </FormControl>
+              </Grid>
+              <Grid container direction="row" justifyContent="center">
+                <Button variant="contained" type="submit">
+                  Register
+                </Button>
+              </Grid>
+            </form>
+          </Grid>
+        </Grid>
+      )}
+    </div>
   )
 }
